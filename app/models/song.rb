5e1f0733ccbde079a6c423aacfa1a26b4ba2a95b
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-24 01:51:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,4 +9,9 @@ class Song < ActiveRecord::Base
   def artist_name
     self.try(:artist).try(:name)
   end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
 end
