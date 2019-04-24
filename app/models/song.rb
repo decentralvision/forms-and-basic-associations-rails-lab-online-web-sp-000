7631ceb050625b295cf05444bebf2c404a5dbb5c
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-24 01:44:04 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  def song_artist_name
-    Artist.find(self.artist_id).name
-  end
+  # def song_artist_name
+  #   Artist.find(self.artist_id).name
+  # end
 end
